313fa726c0dc4413b43873827078a4bf6d43191f
Fernando Poletto <fernandompoletto@gmail.com>
2026-09-14 17:05:18 -0300

    first commit

---
+++ tests/app.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test'
+
+test('deve exibir o slogan na aba do navegador  ', async ({ page }) => {
+  await page.goto('http://localhost:3000/')
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Lunar Pass/)
+})
+
