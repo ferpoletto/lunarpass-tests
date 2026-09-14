774d6d86669cd1c2c2c8f867e99e41caab53832b
Fernando Poletto <fernandompoletto@gmail.com>
2026-09-14 17:16:20 -0300

    Ajustando teste apenas no chrome e validando slogan no cn2

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,9 +1,16 @@
 import { test, expect } from '@playwright/test'
 
-test('deve exibir o slogan na aba do navegador  ', async ({ page }) => {
+test('deve validar o título na aba do navegador', async ({ page }) => {
   await page.goto('http://localhost:3000/')
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Lunar Pass/)
 })
 
+
+test('deve exibir o slogan na aba do navegador', async ({ page }) => {
+  await page.goto('http://localhost:3000/')
+
+  const slogan = page.getByRole('heading', {name: 'Sua viagem para a Lua começa aqui.'})
+  await expect(slogan).toBeVisible()
+})
