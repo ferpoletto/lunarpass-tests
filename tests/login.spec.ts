93dd7d37c7cf07fba66631e3617b8cb11a62e75e
Fernando Poletto <fernandompoletto@gmail.com>
2026-09-15 14:18:33 -0300

    Criando a classe LoginPage

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,18 +1,11 @@
-import {test,expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
+
+import { LoginPage } from '../pages/login.page'
 
 test('deve autenticar no controle de missões', async ({ page }) => {
-    await page.goto('http://localhost:3000/mission-control/login')
-
-    const title = page.getByRole('heading', {name: 'Mission Control'})
-    await expect(title).toBeVisible()
-/*
-    await page.locator('input[type=email]').fill('buzz@lunarpass.dev')
-    await page.locator('input[type=password]').fill('pwd123')
-    await page.getByRole('button', {name: 'Entrar'}).click()
-*/
-    await page.getByLabel('E-mail').fill('buzz@lunarpass.dev')
-    await page.getByLabel('Senha').fill('pwd123')
-    await page.getByRole('button', {name: 'Entrar'}).click()
+    const loginPage = new LoginPage(page)
+    await loginPage.go()
+    await loginPage.login('buzz@lunarpass.dev', 'pwd123')
 
     await page.waitForURL('http://localhost:3000/mission-control')
     const title1 = page.getByRole('heading', {name: 'Lunar Pass'})
@@ -20,74 +13,49 @@ test('deve autenticar no controle de missões', async ({ page }) => {
 
     const logoutButton = page.getByRole('button', {name: 'Sair'})
     await expect(logoutButton).toBeVisible()
-    await page.waitForTimeout(2000)
+
 })
 
 test('não deve autenticar com senha incorreta', async ({ page }) => {
-    await page.goto('http://localhost:3000/mission-control/login')
-
-    const title = page.getByRole('heading', {name: 'Mission Control'})
-    await expect(title).toBeVisible()
-
-    await page.getByLabel('E-mail').fill('buzz@lunarpass.dev')
-    await page.getByLabel('Senha').fill('errado')
-    await page.getByRole('button', {name: 'Entrar'}).click()
+    const loginPage = new LoginPage(page)
+    await loginPage.go()
+    await loginPage.login('buzz@lunarpass.dev', 'pwdaaa123')
 
     const errorMessage = page.locator('p[role=alert]')
     await expect(errorMessage).toHaveText('E-mail ou senha inválidos.')
 })
 
 test('não deve autenticar com e-mail não cadastrado', async ({ page }) => {
-    await page.goto('http://localhost:3000/mission-control/login')
-
-    const title = page.getByRole('heading', {name: 'Mission Control'})
-    await expect(title).toBeVisible()
-
-    await page.getByLabel('E-mail').fill('fernandopoletto@lunarpass.dev')
-    await page.getByLabel('Senha').fill('pwd123')
-    await page.getByRole('button', {name: 'Entrar'}).click()
+    const loginPage = new LoginPage(page)
+    await loginPage.go()
+    await loginPage.login('404@lunarpass.dev', 'pwd123')
 
     const errorMessage = page.locator('p[role=alert]')
     await expect(errorMessage).toHaveText('E-mail ou senha inválidos.')
 })
 
 test('não deve autenticar com e-mail vazio', async ({ page }) => {
-    await page.goto('http://localhost:3000/mission-control/login')
-
-    const title = page.getByRole('heading', {name: 'Mission Control'})
-    await expect(title).toBeVisible()
-
-    //await page.getByLabel('E-mail').fill('fernandopoletto@lunarpass.dev')
-    await page.getByLabel('Senha').fill('pwd123')
-    await page.getByRole('button', {name: 'Entrar'}).click()
+    const loginPage = new LoginPage(page)
+    await loginPage.go()
+    await loginPage.login(' ', 'pwd123')
 
     const errorMessage = page.locator('p[role=alert]')
     await expect(errorMessage).toHaveText('Informe um e-mail válido')
 })
 
 test('não deve autenticar com campos vazios', async ({ page }) => {
-    await page.goto('http://localhost:3000/mission-control/login')
-
-    const title = page.getByRole('heading', {name: 'Mission Control'})
-    await expect(title).toBeVisible()
-
-    //await page.getByLabel('E-mail').fill('fernandopoletto@lunarpass.dev')
-    await page.getByLabel('Senha').fill('pwd123')
-    await page.getByRole('button', {name: 'Entrar'}).click()
+    const loginPage = new LoginPage(page)
+    await loginPage.go()
+    await loginPage.login(' ', ' ')
 
     const errorMessage = page.locator('p[role=alert]')
     await expect(errorMessage).toHaveText('Informe um e-mail válido')
 })
 
-test('não deve autenticar com campos senha vazio', async ({ page }) => {
-    await page.goto('http://localhost:3000/mission-control/login')
-
-    const title = page.getByRole('heading', {name: 'Mission Control'})
-    await expect(title).toBeVisible()
-
-    await page.getByLabel('E-mail').fill('fernandopoletto@lunarpass.dev')
-    //await page.getByLabel('Senha').fill('pwd123')
-    await page.getByRole('button', {name: 'Entrar'}).click()
+test('não deve autenticar com campo senha vazio', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    await loginPage.go()
+    await loginPage.login('buzz@lunarpass.dev', '')
 
     const errorMessage = page.locator('p[role=alert]')
     await expect(errorMessage).toHaveText('Informe a senha')
